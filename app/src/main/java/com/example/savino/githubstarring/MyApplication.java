4344ebbbe3e3530b09d7932b16ef95a652cfd5cc
Savino Ordine <savino.ordine@scmitaly.it>
2017-01-05 09:29:19 +0100

    Create application for component injection

---
+++ app/src/main/java/com/example/savino/githubstarring/MyApplication.java
@@ -0,0 +1,37 @@
+package com.example.savino.githubstarring;
+import android.app.Application;
+
+import com.example.savino.githubstarring.di.component.DaggerListingRepoPresenterComponent;
+import com.example.savino.githubstarring.di.component.ListingRepoPresenterComponent;
+import com.example.savino.githubstarring.di.module.ApiModule;
+
+
+public class MyApplication extends Application {
+
+    private ListingRepoPresenterComponent mListingRepoPresenterComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mListingRepoPresenterComponent = createListingRepoComponent();
+    }
+
+    private ListingRepoPresenterComponent createListingRepoComponent() {
+        return DaggerListingRepoPresenterComponent.builder()
+                .apiModule(new ApiModule())
+                .build();
+    }
+
+    public void setListingRepoPresenterComponent(ListingRepoPresenterComponent component) {
+        mListingRepoPresenterComponent = component;
+    }
+
+    public ListingRepoPresenterComponent getListingRepoPresenterComponent(){
+        if (mListingRepoPresenterComponent != null) {
+            return mListingRepoPresenterComponent;
+        }
+
+        return createListingRepoComponent();
+    }
+}
