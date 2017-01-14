5e94e4699e10c988a0a4eba122aa9a3a926953ae
Savino Ordine <savino.ordine@scmitaly.it>
2017-01-14 14:10:27 +0100

    better dialog UI

--- app/src/main/java/com/example/savino/githubstarring/activities/MainActivity.java
+++ app/src/main/java/com/example/savino/githubstarring/activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.savino.githubstarring.activities;
 
 import android.content.DialogInterface;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -9,7 +11,10 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.example.savino.githubstarring.R;
 import com.example.savino.githubstarring.databinding.ActivityMainBinding;
@@ -48,33 +53,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createDialog() {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this, R.style.dialog);
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainActivity.this, R.style.dialog);
 
         final DialogBinding binding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.dialog, null, false);
 
-        dialog.setView(binding.getRoot());
-        dialog.setTitle("Fill both fields");
-        dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String o = binding.owner.getText().toString();
-                String r = binding.repo.getText().toString();
-                if (!hasEmptyField(o, r) ) {
-                    if (mListener != null) {
-                        mListener.onDialogFilled(o, r);
+        AlertDialog dialog = dialogBuilder.setView(binding.getRoot())
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String o = binding.owner.getText().toString();
+                        String r = binding.repo.getText().toString();
+                        if (!hasEmptyField(o, r)) {
+                            if (mListener != null) {
+                                mListener.onDialogFilled(o, r);
+                            }
+                        }
                     }
-                }
-            }
 
-        });
-        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+
+        DisplayMetrics metrics = dialogBuilder.getContext()
+                .getResources()
+                .getDisplayMetrics();
+
+        final int width = metrics.widthPixels;
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
+            public void onShow(DialogInterface dialog) {
+                AlertDialog alertDialog = (AlertDialog) dialog;
+                View view = alertDialog.getWindow().getDecorView()
+                        .findViewById(android.R.id.content);
+                FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) view.getLayoutParams();
+                layoutParams.width = 4 * width / 5; // 80% of screen
+                layoutParams.gravity = Gravity.CENTER;
+                view.setLayoutParams(layoutParams);
+                alertDialog.getWindow().setBackgroundDrawable(
+                        new ColorDrawable(Color.WHITE));
             }
         });
 
-        dialog.create().show();
+        dialog.show();
+
     }
 
     private boolean hasEmptyField(String o, String r) {
